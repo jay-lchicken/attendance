4adff541d18bbd530d63d309813d5c02b4f687f3
Lai Hong Yu <joyjay606@gmail.com>
2024-12-01 16:01:18 +0800

    Commit

--- app/dashboard/firebase.js
+++ app/dashboard/firebase.js
@@ -1,8 +1,19 @@
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import {setDoc,doc,getFirestore, getDoc, getDocs} from "firebase/firestore";
-import {auth, db, app} from "../firebase-auth";
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export function logout(){
     signOut(auth).then(() => {
         // Sign-out successful.
